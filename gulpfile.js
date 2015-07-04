f04bd5fd16499b8f78b89ef9828b62595031a58c
Prakash Venkat <pv@adcade.com>
2015-07-04 01:59:44 -0400

    more bp changes

--- gulpfile.js
+++ gulpfile.js
@@ -6,14 +6,19 @@ var del = require('del');
 var babel = require('gulp-babel');
 var jade = require('gulp-jade');
 var data = require('gulp-data');
+var stylus = require('gulp-stylus');
 
 var src = {
 	build: [
 	"./src/app/shared/**/*.js", 
 	"./src/app/components/**/*.js",
 	"./src/app/app.js",
-	"./src/assets/**/*.*],
-	jade: ["./src/index.jade"]
+	"./src/assets/**/*.*"],
+	jade: ["./src/index.jade"],
+	fonts: ["./src/assets/**/*.woff"],
+	css: ["./src/assets/**/*.css"],
+	images:["./src/assets/**/*.jpg"],
+	stylus: ["./src/assets/**/*.styl"]
 };
 
 var out = {
@@ -35,17 +40,40 @@ gulp.task('clean', function(cb) {
 // 	.pipe(gulp.dest(out.build));
 // });
 
+gulp.task('font', function() {
+  gulp.src(src.fonts)
+  .pipe(gulp.dest(out.build));
+})
+
+gulp.task('image', function(){
+  gulp.src(src.images)
+  .pipe(gulp.dest(out.build));
+});
+
+gulp.task('copyCss', function() {
+  gulp.src(src.css)
+  .pipe(gulp.dest(out.build));
+});
+
+gulp.task('stylus', function() {
+  gulp.src(src.stylus)
+  .pipe(stylus())
+  .pipe(gulp.dest(out.build));
+});
+
 gulp.task('buildjade', function() {
 	gulp.src(src.jade)
-	.pipe(data(function(file) {
-		return {
-			fileName: path.basename(file.path)
-		}
-	}))
+	//.pipe(data(function(file) {
+	//	return {
+	//		fileName: path.basename(file.path)
+	//	}
+	//}))
 	.pipe(jade({
 		locals: {
 			styletags: [{
-				path: "assets/styles/rug/rug.built.css"
+				path: "styles/rug/rug.built.css"
+			}, {
+			  path: "styles/index.css"
 			}],
 			scripttags: [{
 				path: "https://code.jquery.com/jquery-2.1.4.min.js"
@@ -56,5 +84,5 @@ gulp.task('buildjade', function() {
 	.pipe(gulp.dest(out.build));
 });
 
-gulp.task('build', ['clean']);
-gulp.task('default', ['build', 'buildjade']);
+//gulp.task('build', ['clean']);
+gulp.task('default', ['buildjade', 'stylus', 'copyCss','font','image']);
